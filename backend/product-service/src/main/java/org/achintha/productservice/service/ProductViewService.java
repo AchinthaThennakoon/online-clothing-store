e8524a3a440502204d9423c76feef5d550b70f77
Achintha Thennakoon <achinthathennakoon97@gmail.com>
2024-03-05 10:52:03 +0530

    remove comments

--- backend/product-service/src/main/java/org/achintha/productservice/service/ProductViewService.java
+++ backend/product-service/src/main/java/org/achintha/productservice/service/ProductViewService.java
@@ -6,6 +6,4 @@ import java.util.List;
 
 public interface ProductViewService {
     List<ProductDTO> getProducts(Object requestBody, int page, int size, String[] sort, boolean search);
-
-//    List<ProductDTO> getProducts(Object requestBody, int page, int size, String[] sort, boolean search, String category1);
 }
