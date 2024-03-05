f7f51b1b3afebc207146d5bf841f2552a8e27e6e
Achintha Thennakoon <achinthathennakoon97@gmail.com>
2024-03-05 10:51:18 +0530

    remove findBtCategory method

--- backend/product-service/src/main/java/org/achintha/productservice/repository/ProductRepository.java
+++ backend/product-service/src/main/java/org/achintha/productservice/repository/ProductRepository.java
@@ -1,8 +1,12 @@
 package org.achintha.productservice.repository;
 
 import org.achintha.productservice.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface ProductRepository extends JpaRepository<Product,Long>, JpaSpecificationExecutor<Product> {
+//    Page<Product> findByCategory1(String category1, Pageable pageable);
 }
