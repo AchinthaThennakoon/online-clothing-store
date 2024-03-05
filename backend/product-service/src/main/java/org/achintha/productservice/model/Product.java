71f9ac98b5fab67565aa2d160b583f0510b19465
Achintha Thennakoon <achinthathennakoon97@gmail.com>
2024-03-05 09:57:51 +0530

    add sub categories

--- backend/product-service/src/main/java/org/achintha/productservice/model/Product.java
+++ backend/product-service/src/main/java/org/achintha/productservice/model/Product.java
@@ -22,7 +22,9 @@ public class Product {
     private Long id;
 
     private String name;
-    private String category;
+    private String category1;
+    private String category2;
+    private String category3;
     private Double price;
     private Date createTime;
 
